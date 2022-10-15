92c7c6ba3b792beb2296df72b32f8475172e3557
keaidehuangpi <83892521+keaidehuangpi@users.noreply.github.com>
2022-10-15 23:54:09 +0800

    1.8.9
    Signed-off-by: keaidehuangpi <83892521+keaidehuangpi@users.noreply.github.com>

--- src/main/java/org/teacon/xibao/Xibao.java
+++ src/main/java/org/teacon/xibao/Xibao.java
@@ -1,67 +1,56 @@
 package org.teacon.xibao;
 
-import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.BufferBuilder;
-import com.mojang.blaze3d.vertex.DefaultVertexFormat;
-import com.mojang.blaze3d.vertex.Tesselator;
-import com.mojang.blaze3d.vertex.VertexFormat;
-import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.gui.screens.DisconnectedScreen;
-import net.minecraft.network.chat.TranslatableComponent;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.ScreenEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.IExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiDisconnected;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.WorldRenderer;
+import net.minecraft.client.renderer.texture.TextureManager;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.ForgeEventFactory;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.loading.FMLPaths;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
+@Mod(modid = Xibao.MODID, version = Xibao.VERSION)
+public class Xibao
+{
 
-@Mod("xibao")
-public class Xibao {
-    public Xibao() {
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class,
-            () -> new IExtensionPoint.DisplayTest(() -> "ANY", (a, b) -> b));
+    public static final String MODID = "examplemod";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        MinecraftForge.EVENT_BUS.register(this);
     }
+    private static final ResourceLocation LOCATION = new ResourceLocation("xibao", "textures/xibao.png");
+    @SubscribeEvent
+    public void onDisconnect(GuiScreenEvent.BackgroundDrawnEvent e){
 
-    @Mod.EventBusSubscriber(modid = "xibao", value = Dist.CLIENT)
-    public static final class XibaoImpl {
-        private static final ResourceLocation LOCATION = new ResourceLocation("xibao", "textures/xibao.png");
-        @SubscribeEvent
-        public static void on(ScreenEvent.InitScreenEvent.Post event) {
-            var showXibao = !Files.exists(FMLPaths.GAMEDIR.get().resolve(".xibao_stop"));
-            if (showXibao && event.getScreen() instanceof DisconnectedScreen s) {
-                var disableXibao = new Button(s.width / 2 - 75, s.height - 30, 150, 20, new TranslatableComponent("xibao.do_not_show_again"), btn -> {
-                    var gameDir = FMLPaths.GAMEDIR.get();
-                    try {
-                        Files.writeString(gameDir.resolve(".xibao_stop"), "Remove this file to show Xibao again", StandardCharsets.UTF_8);
-                    } catch (IOException e) {
-                        return;
-                    }
-                    btn.active = false;
-                });
-                event.addListener(disableXibao);
-            }
-        }
+        if (e.gui instanceof GuiDisconnected){
+            GuiDisconnected s= ((GuiDisconnected) e.gui);
+            Minecraft.getMinecraft().getTextureManager().bindTexture(LOCATION);
+            Tessellator tessellator = Tessellator.getInstance();
+            WorldRenderer worldrenderer = tessellator.getWorldRenderer();
+            //Minecraft.getMinecraft().getTextureManager().bindTexture(optionsBackground);
+            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
 
-        @SubscribeEvent
-        public static void on(ScreenEvent.BackgroundDrawnEvent event) {
-            var showXibao = !Files.exists(FMLPaths.GAMEDIR.get().resolve(".xibao_stop"));
-            if (showXibao && event.getScreen() instanceof DisconnectedScreen s) {
-                Tesselator tesselator = Tesselator.getInstance();
-                BufferBuilder bufferbuilder = tesselator.getBuilder();
-                RenderSystem.setShaderTexture(0, LOCATION);
-                bufferbuilder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX_COLOR);
-                bufferbuilder.vertex(0.0D, s.height, 0.0D).uv(0F, 1F).color(255, 255, 255, 255).endVertex();
-                bufferbuilder.vertex(s.width, s.height, 0.0D).uv(1F, 1F).color(255, 255, 255, 255).endVertex();
-                bufferbuilder.vertex(s.width, 0.0D, 0.0D).uv(1F, 0F).color(255, 255, 255, 255).endVertex();
-                bufferbuilder.vertex(0.0D, 0.0D, 0.0D).uv(0F, 0F).color(255, 255, 255, 255).endVertex();
-                tesselator.end();
-            }
+            worldrenderer.begin(7, DefaultVertexFormats.POSITION_TEX_COLOR);
+            worldrenderer.pos(0.0D, s.height, 0.0D).tex(0F, 1F).color(255, 255, 255, 255).endVertex();
+            worldrenderer.pos(s.width, s.height, 0.0D).tex(1F, 1F).color(255, 255, 255, 255).endVertex();
+            worldrenderer.pos(s.width, 0.0D, 0.0D).tex(1F, 0F).color(255, 255, 255, 255).endVertex();
+            worldrenderer.pos(0.0D, 0.0D, 0.0D).tex(0F, 0F).color(255, 255, 255, 255).endVertex();
+            tessellator.draw();
+
+
         }
     }
 }
